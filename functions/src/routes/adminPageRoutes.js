2e2d1f43d839c68314951962b71263514cf99947
Sandy3122 <seeram87@gmail.com>
2024-03-24 00:09:11 +0530

    Fixed ;ogin routes issue

--- functions/src/routes/adminPageRoutes.js
+++ functions/src/routes/adminPageRoutes.js
@@ -4,21 +4,25 @@ const path = require("path");
 
 // Middleware to check if user is admin
 const isAdmin = (req, res, next) => {
-  if (req.session.role === "admin") {
+  if (req.session && req.session.token && req.session.role === "admin") {
     // User is admin, proceed to the next middleware
     next();
   } else {
     // User is not admin, redirect to access-denied route
-    res.redirect("/api/access-denied");
+    res.redirect("/admin/admin-login");
   }
 };
 
 // Admin Login route
 router.get("/admin-login", (req, res, next) => {
   // Check if user is authenticated
-  if (req.session.token) {
+  if (req.session && req.session.token && req.session.role === 'admin') {
     // If authenticated, redirect or any authenticated route
     return res.redirect("/admin/dashboard");
+  }
+  if (req.session && req.session.token && req.session.role !== 'admin') {
+    // If an admin, show an appropriate error message
+    return res.status(403).send("Access denied. Other login session is already active");
   }
   // If not authenticated, serve the login page
   res.sendFile(path.join(__dirname, "..", "..", "public", "admin", "adminLogin.html"));
@@ -35,7 +39,7 @@ router.get('/dashboard', isAdmin, (req, res) => {
 });
 
 // Define route to serve access rights page
-router.get('/access-rights', isAdmin, (req, res) => {
+router.get('/access-rights', (req, res) => {
   res.sendFile(path.join(__dirname, '..', '..', 'public', 'employees', 'accessRights.html'));
 });
 
--- functions/src/routes/employeePageRoutes.js
+++ functions/src/routes/employeePageRoutes.js
@@ -6,11 +6,12 @@ const path = require("path");
 
 // Middleware to check if user is an employee
 const isEmployee = (req, res, next) => {
-  if (req.session.role !== "admin") {
-    // User is an employee, proceed to the next middleware
+  // Check if user is authenticated and the role is employee
+  if (req.session && req.session.token && req.session.role !== "admin") {
+    // User is authenticated as an employee, proceed to the next middleware
     next();
   } else {
-    // User is not an employee, redirect to access-denied route or any other route as needed
+    // User is not authenticated as an employee, redirect to login page
     res.redirect("/api/access-denied");
   }
 };
@@ -20,24 +21,32 @@ router.get("/employee-registration", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "..", "public", "employees", "employeeRegistration.html"));
 });
 
-// Employee Login route
+// A Login route
 router.get("/employee-login", (req, res) => {
   // Check if user is authenticated
-  if (req.session.token) {
-    // If authenticated, redirect to employee Search route
+  if (req.session && req.session.token && req.session.role !== 'admin') {
+    // If authenticated and not an admin, redirect to employee dashboard
     return res.redirect("/employee/dashboard");
   }
-  // If not authenticated, redirect to the login page  
+
+  // Check if the user is an admin
+  if (req.session && req.session.role === 'admin') {
+    // If an admin, show an appropriate error message
+    return res.status(403).send("Access denied. Other login session is already active");
+  }
+
+  // If not authenticated or not an admin, serve the employee login page
   res.sendFile(path.join(__dirname, "..", "..", "public", "employees", "employeeLogin.html"));
 });
 
 // Define route to serve employee profile page
 router.get('/dashboard', isEmployee, (req, res) => {
-    // Check if user is authenticated
-    if (!req.session.token) {
-      // If not authenticated, redirect to login page
-      return res.redirect("/employee/employee-login");
-    }
+  // Check if user is authenticated
+  if (!req.session.token) {
+    // If not authenticated, redirect to login page
+    return res.redirect("/employee/employee-login");
+  }
+  // If authenticated and an employee, serve the main dashboard page
   res.sendFile(path.join(__dirname, '..', '..', 'public', 'main.html'));
 });
 
